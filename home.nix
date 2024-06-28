2920ca1783ce3ff5c44f348c03688d34e3c9a232
Alex Jercan <jercan_alex27@yahoo.com>
2024-06-28 09:34:39 +0300

    asdasd

--- home.nix
+++ home.nix
@@ -37,7 +37,6 @@
     pwvucontrol
     ripgrep
     scrot
-    tmux
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
