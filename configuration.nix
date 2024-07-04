3aa00b8fcc7998edd45bdaab2e4f8b62018f3c25
Alex Jercan <jercan_alex27@yahoo.com>
2024-07-04 14:15:47 +0300

    fmt

--- configuration.nix
+++ configuration.nix
@@ -132,18 +132,21 @@
 
   virtualisation.libvirtd = {
     enable = true;
-  qemu = {
-    package = pkgs.qemu_kvm;
-    runAsRoot = true;
-    swtpm.enable = true;
-    ovmf = {
-      enable = true;
-      packages = [(pkgs.OVMF.override {
-        secureBoot = true;
-        tpmSupport = true;
-      }).fd];
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          })
+          .fd
+        ];
+      };
     };
-  };
   };
 
   # Allow unfree packages
