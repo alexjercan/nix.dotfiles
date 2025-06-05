063bea261dc9b432b50014453652a4e78eef2eb3
Alex Jercan <jercan_alex27@yahoo.com>
2025-06-05 22:01:36 +0300

    feat: update to latest

--- neovim/plugins/harpoon.nix
+++ neovim/plugins/harpoon.nix
@@ -2,19 +2,7 @@
   programs.nixvim = {
     plugins.harpoon = {
       enable = true;
-
-      keymapsSilent = true;
-
-      keymaps = {
-        addFile = "<leader>H";
-        toggleQuickMenu = "<leader>h";
-        navFile = {
-          "1" = "<leader>1";
-          "2" = "<leader>2";
-          "3" = "<leader>3";
-          "4" = "<leader>4";
-        };
-      };
+      enableTelescope = true;
     };
   };
 }
