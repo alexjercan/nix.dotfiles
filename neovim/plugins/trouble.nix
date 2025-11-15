82a5bcb368a94e2d3abd2ccef5e719735667a0ca
Alex Jercan <jercan_alex27@yahoo.com>
2025-11-15 09:39:10 +0200

    fix: update trouble and add open with for telescope

--- neovim/plugins/trouble.nix
+++ neovim/plugins/trouble.nix
@@ -2,38 +2,57 @@
   programs.nixvim = {
     plugins.trouble = {
       enable = true;
+      luaConfig.post = ''
+        local open_with_trouble = require("trouble.sources.telescope").open
+        local telescope = require("telescope")
+
+        telescope.setup({
+          defaults = {
+            mappings = {
+              i = { ["<c-t>"] = open_with_trouble },
+              n = { ["<c-t>"] = open_with_trouble },
+            },
+          },
+        })
+      '';
     };
 
     keymaps = [
       {
         mode = "n";
         key = "<leader>xx";
-        action = "<cmd>Trouble<CR>";
-        options = {desc = "Toggle Treesitter Context";};
+        action = "<cmd>Trouble diagnostics toggle<cr>";
+        options = {desc = "Diagnostics (Trouble)";};
       }
       {
         mode = "n";
-        key = "<leader>xw";
-        action = "<cmd>Trouble workspace_diagnostics<CR>";
-        options = {desc = "Toggle Treesitter Context";};
+        key = "<leader>xX";
+        action = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
+        options = {desc = "Buffer Diagnostics (Trouble)";};
       }
       {
         mode = "n";
-        key = "<leader>xd";
-        action = "<cmd>Trouble document_diagnostics<CR>";
-        options = {desc = "Toggle Treesitter Context";};
+        key = "<leader>cs";
+        action = "<cmd>Trouble symbols toggle focus=false<cr>";
+        options = {desc = "Symbols (Trouble)";};
       }
       {
         mode = "n";
-        key = "<leader>xq";
-        action = "<cmd>Trouble quickfix<CR>";
-        options = {desc = "Toggle Treesitter Context";};
+        key = "<leader>cl";
+        action = "<cmd>Trouble lsp toggle focus=false win.position=right<cr>";
+        options = {desc = "LSP Definitions / references / ... (Trouble)";};
       }
       {
         mode = "n";
-        key = "<leader>xl";
-        action = "<cmd>Trouble loclist<CR>";
-        options = {desc = "Toggle Treesitter Context";};
+        key = "<leader>xL";
+        action = "<cmd>Trouble loclist toggle<cr>";
+        options = {desc = "Location List (Trouble)";};
+      }
+      {
+        mode = "n";
+        key = "<leader>xQ";
+        action = "<cmd>Trouble qflist toggle<cr>";
+        options = {desc = "Quickfix List (Trouble)";};
       }
     ];
   };
