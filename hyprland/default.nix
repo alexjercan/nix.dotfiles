648da0062247a62c4cfea709fdb42366a5401fbc
Alex Jercan <jercan_alex27@yahoo.com>
2025-11-26 19:55:32 +0200

    fix: add 10th workspace

--- hyprland/default.nix
+++ hyprland/default.nix
@@ -160,17 +160,16 @@
           "$mod,       W, exec, bash -c \"kill -SIGUSR1 $(pidof waybar)\""
         ]
         ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
           builtins.concatLists (builtins.genList (
               i: let
                 ws = i + 1;
+                key = toString i;
               in [
-                "$mod, code:1${toString i}, workspace, ${toString ws}"
-                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+                "$mod, code:1${key}, workspace, ${toString ws}"
+                "$mod SHIFT, code:1${key}, movetoworkspace, ${toString ws}"
               ]
             )
-            9)
+            10)
         );
     };
   };
