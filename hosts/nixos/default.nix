8ebba4968b6039b20487373c54b86805aaeb4461
Alex Jercan <jercan_alex27@yahoo.com>
2026-06-18 15:18:52 +0300

    fix: add smb back

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -250,6 +250,36 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+      "public" = {
+        "path" = "/home/alex/Shared/public";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "alex";
+
+        "fruit:aapl" = "yes";
+        "fruit:time machine" = "yes";
+        "vfs objects" = "catia fruit streams_xattr";
+      };
+    };
+  };
+
   services.ollama = {
     package = pkgs.ollama-cuda;
     enable = true;
